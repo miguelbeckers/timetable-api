8c657ee6d3288180b0f5200767705d4876de5421
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2023-05-25 08:23:09 +0100

    Initial commit

---
+++ src/main/java/ipb/pt/timetableapi/TimetableApiApplication.java
@@ -0,0 +1,13 @@
+package ipb.pt.timetableapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipb/pt/timetableapi/TimetableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ipb.pt.timetableapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
