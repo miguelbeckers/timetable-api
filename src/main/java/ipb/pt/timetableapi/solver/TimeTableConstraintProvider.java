917ff7f064a5da3db759856bfdcaaacaa61b7955
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2023-11-20 13:52:05 +0000

    implemented student and availability conflicts

--- src/main/java/ipb/pt/timetableapi/solver/TimeTableConstraintProvider.java
+++ src/main/java/ipb/pt/timetableapi/solver/TimeTableConstraintProvider.java
@@ -1,8 +1,6 @@
 package ipb.pt.timetableapi.solver;
 
-import ipb.pt.timetableapi.model.Lesson;
-import ipb.pt.timetableapi.model.LessonStudent;
-import ipb.pt.timetableapi.model.Student;
+import ipb.pt.timetableapi.model.*;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
@@ -21,6 +19,10 @@ public class TimeTableConstraintProvider implements ConstraintProvider {
                 roomConflict(constraintFactory),
                 studentConflict(constraintFactory),
                 professorConflict(constraintFactory),
+                resourceAvailability(constraintFactory), // missing test
+                classroomAvailability(constraintFactory), // missing test
+                professorAvailability(constraintFactory), // missing test
+                courseAvailability(constraintFactory), // missing test
                 // Soft constraints
                 professorTimeEfficiency(constraintFactory)
         };
@@ -89,5 +91,119 @@ public class TimeTableConstraintProvider implements ConstraintProvider {
                 })
                 .reward(HardSoftScore.ONE_SOFT)
                 .asConstraint("Teacher time efficiency");
+    }
+
+    private Constraint resourceAvailability(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .filter(lesson -> !lesson.getLessonResources().isEmpty()) // Only for lessons with resources
+                .ifExists(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getClassroom),
+                        Joiners.lessThan(Lesson::getId))
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("Resource availability");
+    }
+
+    private int calculateResourceShortage(Lesson lesson, Lesson conflictingLesson) {
+        int shortage = 0;
+        for (LessonResource lessonResource : lesson.getLessonResources()) {
+            Resource resource = lessonResource.getResource();
+            int requiredQuantity = lesson.getLessonStudents().size(); // Number of students in the lesson
+            int availableQuantity = getAvailableQuantity(resource, lesson.getClassroom());
+            int conflictingQuantity = getRequiredQuantityInConflictingLesson(resource, conflictingLesson);
+            int totalRequiredQuantity = requiredQuantity + conflictingQuantity;
+            int shortfall = Math.max(0, totalRequiredQuantity - availableQuantity);
+            shortage += shortfall;
+        }
+        return shortage;
+    }
+
+    private int getAvailableQuantity(Resource resource, Classroom classroom) {
+        // Gets the available quantity of the resource in the classroom
+        for (ClassroomResource classroomResource : classroom.getClassroomResources()) {
+            if (classroomResource.getResource().equals(resource)) {
+                return classroomResource.getQuantity();
+            }
+        }
+        return 0;
+    }
+
+    private int getRequiredQuantityInConflictingLesson(Resource resource, Lesson conflictingLesson) {
+        // Gets the required quantity of the resource in the conflicting lesson
+        int conflictingQuantity = 0;
+        for (LessonResource conflictingLessonResource : conflictingLesson.getLessonResources()) {
+            if (conflictingLessonResource.getResource().equals(resource)) {
+                conflictingQuantity += conflictingLesson.getLessonStudents().size();
+            }
+        }
+        return conflictingQuantity;
+    }
+
+    private Constraint classroomAvailability(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .filter(lesson -> lesson.getClassroom() != null) // Only for lessons with an assigned classroom
+                .ifExists(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.lessThan(Lesson::getId))
+                .filter(this::hasClassroomUnavailabilityConflict)
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("Classroom unavailability conflict");
+    }
+
+    private boolean hasClassroomUnavailabilityConflict(Lesson conflictingLesson) {
+        Classroom classroom = conflictingLesson.getClassroom();
+        List<Timeslot> lessonUnavailability = classroom.getUnavailability();
+        return hasUnavailabilityConflict(conflictingLesson, lessonUnavailability);
+    }
+
+    private boolean hasUnavailabilityConflict(Lesson conflictingLesson, List<Timeslot> unavailabilityList) {
+        if (unavailabilityList != null && !unavailabilityList.isEmpty()) {
+            Timeslot lessonTimeslot = conflictingLesson.getTimeslot();
+            for (Timeslot unavailabilityTimeslot : unavailabilityList) {
+                if (unavailabilityTimeslot.equals(lessonTimeslot)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Constraint professorAvailability(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .filter(lesson -> lesson.getProfessor() != null) // Only for lessons with an assigned professor
+                .ifExists(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.lessThan(Lesson::getId))
+                .filter(this::hasProfessorUnavailabilityConflict)
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("Professor unavailability conflict");
+    }
+
+    private boolean hasProfessorUnavailabilityConflict(Lesson conflictingLesson) {
+        Professor professor = conflictingLesson.getProfessor();
+        List<Timeslot> professorUnavailability = professor.getUnavailability();
+        return hasUnavailabilityConflict(conflictingLesson, professorUnavailability);
+    }
+
+    private Constraint courseAvailability(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .filter(lesson -> lesson.getSubjectCourse() != null) //
+                .ifExists(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.lessThan(Lesson::getId))
+                .filter(this::hasCourseUnavailabilityConflict)
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("Course unavailability conflict");
+    }
+
+    private boolean hasCourseUnavailabilityConflict(Lesson conflictingLesson) {
+        SubjectCourse subjectCourse = conflictingLesson.getSubjectCourse();
+        Course course = subjectCourse.getCourse();
+        List<Timeslot> courseUnavailability = course.getUnavailability();
+        return hasUnavailabilityConflict(conflictingLesson, courseUnavailability);
     }
 }
