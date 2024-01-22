63a67342b6e69d069dc72c2e64732274728457eb
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-01-22 17:13:37 +0000

    implementing DataController

--- src/main/java/ipb/pt/timetableapi/controller/ClassroomController.java
+++ src/main/java/ipb/pt/timetableapi/controller/ClassroomController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,9 @@ public class ClassroomController {
 
     @Autowired
     private ClassroomService classroomService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
@@ -40,7 +44,7 @@ public class ClassroomController {
     }
 
     @PostMapping()
-    public ResponseEntity<Object> create(@Valid @RequestBody ClassroomDto classroomDto) {
+    public ResponseEntity<Object> create(@RequestBody ClassroomDto classroomDto) {
         Classroom classroom = new Classroom();
         BeanUtils.copyProperties(classroomDto, classroom);
         return ResponseEntity.ok().body(classroomService.create(classroom));
@@ -48,7 +52,7 @@ public class ClassroomController {
 
     @Transactional
     @PostMapping("/many")
-    public ResponseEntity<List<Object>> createMany(@Valid @RequestBody List<ClassroomDto> classroomDtoList) {
+    public ResponseEntity<List<Object>> createMany(@RequestBody List<ClassroomDto> classroomDtoList) {
 
         List<Object> created = new ArrayList<>();
         for (ClassroomDto classroomDto : classroomDtoList) {
@@ -61,7 +65,7 @@ public class ClassroomController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Object> update(@Valid @RequestBody ClassroomDto classroomDto, @PathVariable Long id) {
+    public ResponseEntity<Object> update(@RequestBody ClassroomDto classroomDto, @PathVariable Long id) {
         Optional<Classroom> optional = classroomService.findById(id);
         if (optional.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -80,6 +84,29 @@ public class ClassroomController {
         }
 
         classroomService.delete(areaOptional.get());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/load")
+    public ResponseEntity<Object> load() {
+        ResponseEntity<ClassroomDto[]> responseEntity = restTemplate.getForEntity(
+                "http://localhost:8081/classrooms", ClassroomDto[].class);
+
+        ClassroomDto[] classroomDtos = responseEntity.getBody();
+        List<Classroom> classrooms = new ArrayList<>();
+
+        assert classroomDtos != null;
+        for (ClassroomDto classroomDto : classroomDtos) {
+            Classroom classroom = new Classroom();
+            BeanUtils.copyProperties(classroomDto, classroom);
+            classrooms.add(classroom);
+        }
+
+        for (Classroom classroom : classrooms) {
+            System.out.println(classroom.getName());
+        }
+
+//        return ResponseEntity.ok().body(classroomService.saveAll(classrooms));
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ipb/pt/timetableapi/controller/DataController.java
+++ src/main/java/ipb/pt/timetableapi/controller/DataController.java
@@ -1,5 +1,10 @@
 package ipb.pt.timetableapi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import ipb.pt.timetableapi.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +13,112 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/data")
 public class DataController {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static final String URL = "http://localhost:8081";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @GetMapping("/load")
-    public void load() {
-        ResponseEntity<String> data = restTemplate.exchange("http://localhost:8081/lessons", HttpMethod.GET, null, String.class);
-        System.out.println(data.getBody());
+    public ResponseEntity<Object> load() throws JsonProcessingException {
+        objectMapper.registerModule(new JavaTimeModule());
+
+        ResponseEntity<String> classroomData = restTemplate.exchange(
+                URL + "/classrooms", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> classroomResourceData = restTemplate.exchange(
+                URL + "/classroom-resources", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> classroomTypeData = restTemplate.exchange(
+                URL + "/classroom-types", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> courseData = restTemplate.exchange(
+                URL + "/courses", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> departmentData = restTemplate.exchange(
+                URL + "/departments", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> lessonData = restTemplate.exchange(
+                URL + "/lessons", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> lessonResourceData = restTemplate.exchange(
+                URL + "/lesson-resources", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> lessonUnitData = restTemplate.exchange(
+                URL + "/lesson-units", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> periodData = restTemplate.exchange(
+                URL + "/periods", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> professorData = restTemplate.exchange(
+                URL + "/professors", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> resourceData = restTemplate.exchange(
+                URL + "/resources", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> studentData = restTemplate.exchange(
+                URL + "/students", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> subjectCourseData = restTemplate.exchange(
+                URL + "/subject-courses", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> subjectData = restTemplate.exchange(
+                URL + "/subjects", HttpMethod.GET, null, String.class);
+
+        ResponseEntity<String> timeslotData = restTemplate.exchange(
+                URL + "/timeslots", HttpMethod.GET, null, String.class);
+
+        List<ClassroomDto> classroomDtos = objectMapper.readValue(classroomData.getBody(), new TypeReference<>() {
+        });
+
+        List<ClassroomResourceDto> classroomResourceDtos = objectMapper.readValue(classroomResourceData.getBody(), new TypeReference<>() {
+        });
+
+        List<ClassroomTypeDto> classroomTypeDtos = objectMapper.readValue(classroomTypeData.getBody(), new TypeReference<>() {
+        });
+
+        List<CourseDto> courseDtos = objectMapper.readValue(courseData.getBody(), new TypeReference<>() {
+        });
+
+        List<DepartmentDto> departmentDtos = objectMapper.readValue(departmentData.getBody(), new TypeReference<>() {
+        });
+
+        List<LessonDto> lessonDtos = objectMapper.readValue(lessonData.getBody(), new TypeReference<>() {
+        });
+
+        List<LessonResourceDto> lessonResourceDtos = objectMapper.readValue(lessonResourceData.getBody(), new TypeReference<>() {
+        });
+
+        List<LessonUnitDto> lessonUnitDtos = objectMapper.readValue(lessonUnitData.getBody(), new TypeReference<>() {
+        });
+
+        List<PeriodDto> periodDtos = objectMapper.readValue(periodData.getBody(), new TypeReference<>() {
+        });
+
+        List<ProfessorDto> professorDtos = objectMapper.readValue(professorData.getBody(), new TypeReference<>() {
+        });
+
+        List<ResourceDto> resourceDtos = objectMapper.readValue(resourceData.getBody(), new TypeReference<>() {
+        });
+
+        List<StudentDto> studentDtos = objectMapper.readValue(studentData.getBody(), new TypeReference<>() {
+        });
+
+        List<SubjectCourseDto> subjectCourseDtos = objectMapper.readValue(subjectCourseData.getBody(), new TypeReference<>() {
+        });
+
+        List<SubjectDto> subjectDtos = objectMapper.readValue(subjectData.getBody(), new TypeReference<>() {
+        });
+
+        List<TimeslotDto> timeslotDtos = objectMapper.readValue(timeslotData.getBody(), new TypeReference<>() {
+        });
+
+        return ResponseEntity.ok().build();
     }
 }
