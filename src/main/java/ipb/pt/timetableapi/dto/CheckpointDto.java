aba344375d923659e48df3fc43a3d782fa02d74d
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-03-08 22:25:26 +0000

    added problemId to checkpoint

--- src/main/java/ipb/pt/timetableapi/dto/CheckpointDto.java
+++ src/main/java/ipb/pt/timetableapi/dto/CheckpointDto.java
@@ -3,10 +3,12 @@ package ipb.pt.timetableapi.dto;
 import lombok.Data;
 
 import java.time.LocalTime;
+import java.util.UUID;
 
 @Data
 public class CheckpointDto {
     private Long id;
+    private UUID problemId;
     private LocalTime startTime;
     private LocalTime endTime;
     private int hardScore;
--- src/main/java/ipb/pt/timetableapi/model/Checkpoint.java
+++ src/main/java/ipb/pt/timetableapi/model/Checkpoint.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -15,6 +16,7 @@ import java.time.LocalTime;
 public class Checkpoint {
     @Id
     private Long id;
+    private UUID problemId;
     private LocalTime startTime;
     private LocalTime endTime;
     private int hardScore;
