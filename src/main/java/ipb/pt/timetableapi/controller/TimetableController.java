6e735c69e60faa88603f5422850e6b9ac58eadbd
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-03-11 18:46:25 +0000

    rolled back the TimetableController

--- src/main/java/ipb/pt/timetableapi/controller/TimetableController.java
+++ src/main/java/ipb/pt/timetableapi/controller/TimetableController.java
@@ -1,25 +1,19 @@
 package ipb.pt.timetableapi.controller;
 
-import ipb.pt.timetableapi.dto.StatusDto;
 import ipb.pt.timetableapi.model.Timetable;
 import ipb.pt.timetableapi.repository.ClassroomRepository;
 import ipb.pt.timetableapi.repository.LessonUnitRepository;
 import ipb.pt.timetableapi.repository.TimeslotRepository;
 import ipb.pt.timetableapi.solver.TimetableConstraintConfiguration;
-import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.solver.SolverJob;
 import org.optaplanner.core.api.solver.SolverManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.optaplanner.core.api.score.ScoreManager;
 
-import java.time.Duration;
-import java.time.LocalTime;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -29,15 +23,7 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/timetables")
 public class TimetableController {
     private static final UUID problemId = UUID.randomUUID();
-    private LocalTime startTime = null;
-    private LocalTime lastStartTime = null;
-    private Duration lastDuration = null;
-    private SolverJob<Timetable, UUID> solverJob = null;
-
-//    private Duration spendTimeExpected = Duration.ofHours(10);
-
     private final SolverManager<Timetable, UUID> solverManager;
-
     private final ClassroomRepository classroomRepository;
     private final LessonUnitRepository lessonUnitRepository;
     private final TimeslotRepository timeslotRepository;
@@ -57,96 +43,29 @@ public class TimetableController {
 
     @PostMapping("/solve")
     public ResponseEntity<Object> solve() {
-        if (startTime != null) {
-            return ResponseEntity.status(409).body("Solver is already running.");
-        }
+        TimetableConstraintConfiguration timetableConfiguration = new TimetableConstraintConfiguration();
 
-        Timetable problem = createProblem();
-        solverJob = solverManager.solve(problemId, problem);
-        startTime = LocalTime.now();
-
-        return runAndAnswer(true);
-    }
-
-    @PostMapping("/solve-new")
-    public ResponseEntity<Object> solveNew() {
-        if (startTime != null) {
-            return ResponseEntity.status(409).body("Solver is already running.");
-        }
-
-        Timetable problem = createProblem();
-        solverJob = solverManager.solve(problemId, problem);
-        startTime = LocalTime.now();
-
-        return runAndAnswer(true);
-    }
-
-    @GetMapping("/status")
-    public ResponseEntity<Object> status() {
-        HardSoftScore initialScore = computeScore(createProblem());
-        StatusDto status = new StatusDto();
-        status.setInitialScore(initialScore.toString());
-
-        if (startTime == null) {
-            if (lastStartTime != null && lastDuration != null) {
-                status.setStartTime(lastStartTime.toString());
-                status.setSeconds(lastDuration.toSeconds());
-                status.setStatus("Completed");
-            } else {
-                status.setStatus("Not started");
-            }
-        } else {
-            status.setStartTime(startTime.toString());
-            status.setSeconds(Duration.between(startTime, LocalTime.now()).toSeconds());
-            status.setStatus("Running");
-        }
-
-        return ResponseEntity.ok().body(status);
-    }
-
-    @PostMapping("/stop")
-    public ResponseEntity<Object> stop() {
-        if (startTime == null) {
-            return ResponseEntity.status(404).body("Solver is not running.");
-        }
-
-        solverManager.terminateEarly(problemId);
-
-        return runAndAnswer(false);
-    }
-
-    private ResponseEntity<Object> runAndAnswer(boolean save) {
-        try {
-            Timetable solution = solverJob.getFinalBestSolution();
-            if (save) lessonUnitRepository.saveAll(solution.getLessonUnits());
-            return ResponseEntity.ok().body("Solving completed with score: " + solution.getScore());
-        } catch (InterruptedException | ExecutionException e) {
-            throw new IllegalStateException("Solving failed.", e);
-        } finally {
-            if (startTime != null) {
-                lastStartTime = startTime;
-                lastDuration = Duration.between(startTime, LocalTime.now());
-                startTime = null;
-            }
-        }
-    }
-
-    private Timetable createProblem() {
         Timetable problem = new Timetable();
         problem.setClassrooms(classroomRepository.findAll());
         problem.setTimeslots(timeslotRepository.findAll());
         problem.setLessonUnits(lessonUnitRepository.findAll());
-        TimetableConstraintConfiguration timetableConfiguration = new TimetableConstraintConfiguration();
-
-        timetableConfiguration.setRoomConflict(HardSoftScore.ofHard(10));
-
         problem.setTimetableConfiguration(timetableConfiguration);
-        return problem;
+
+        SolverJob<Timetable, UUID> solverJob = solverManager.solve(problemId, problem);
+        Timetable solution;
+
+        try {
+            solution = solverJob.getFinalBestSolution();
+            lessonUnitRepository.saveAll(solution.getLessonUnits());
+            return ResponseEntity.ok().body("Solving completed with score: " + solution.getScore());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException("Solving failed.", e);
+        }
     }
 
-    private HardSoftScore computeScore(Timetable currentProblem) {
-        ScoreManager<Timetable, HardSoftScore> scoreManager = ScoreManager.create(solverManager);
-        scoreManager.updateScore(currentProblem);
-        return currentProblem.getScore();
+    @PostMapping("/stop")
+    public ResponseEntity<Object> stop() {
+        solverManager.terminateEarly(problemId);
+        return ResponseEntity.ok().build();
     }
 }
