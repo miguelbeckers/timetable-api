b3a02d1eec0ab15f661a60eb5fdab563ae8c9b0f
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-03-11 18:42:39 +0000

    removed checkpoint

--- src/main/java/ipb/pt/timetableapi/repository/CheckpointRepository.java
+++
@@ -1,9 +0,0 @@
-package ipb.pt.timetableapi.repository;
-
-import ipb.pt.timetableapi.model.Checkpoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CheckpointRepository extends JpaRepository<Checkpoint, Long> {
-}
--- src/main/java/ipb/pt/timetableapi/repository/ConstraintScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package ipb.pt.timetableapi.repository;
-
-import ipb.pt.timetableapi.model.ConstraintScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConstraintScoreRepository extends JpaRepository<ConstraintScore, Long> {
-}
